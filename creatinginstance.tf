d2104c87368cbf1d02852353b8dff78480cea81e
Deva <kbdevaprasadkb22@gmail.com>
2021-08-13 07:17:58 +0530

    Env variables

--- creatinginstance.tf
+++ creatinginstance.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-    access_key ="AKIAUIJYE7OABXBN75GA"
-    secret_key =""
-    region     ="us-east-2"
-}
 resource "aws_instance" "MyFirst" {
     ami           ="ami-00399ec92321828f5"
     instance_type ="t2.micro"
